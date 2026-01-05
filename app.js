f3b1ee8fc01ddd59faeb5f4052aed27e52f81869
Vuneco <79581846+Vuneco@users.noreply.github.com>
2026-01-06 00:16:35 +0100

    Update Google Apps Script endpoint URL

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // 1) HIER deine Apps Script Web-App URL eintragen:
-const ENDPOINT = "https://script.google.com/macros/s/AKfycbxCs0vAbmjDcB8LI9umJQcqcjsXl--moDwrknx8ufaQUhe2bBInCzq3t7RJqBF7Gx1h/exec";
+const ENDPOINT = "https://script.google.com/macros/s/AKfycbxwMY6ROIfNlEqojlCIo0hLf580QUbpT2mzh2a-V1lTRqtebr6rE7L_ukYkuU-vGCKB/exec";
 const pinGate = document.getElementById("pinGate");
 const formCard = document.getElementById("formCard");
 const pinInput = document.getElementById("pin");
